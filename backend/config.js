7f15e7c0097112da566c11bafc180e36cfc08325
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-07-06 00:24:19 +0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://arbab02:arbab02@cluster0.m3fklkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
